7ca5eef2f6798c3bc5fb8e8d3e987e333375b6f6
KuchkoNikita <kuchko.n.study@gmail.com>
2023-03-30 13:19:06 +0300

    build commit

--- UI/components/FormComponents/TextFieldGroup/utils/constant.js
+++ UI/components/FormComponents/TextFieldGroup/utils/constant.js
@@ -1,22 +1,29 @@
-const TEXTFIELD_BELPOST_DATA = [
+const DEFAULT_TEXTFIELD_DATA = [
   { id: "name", title: "Фамилия Имя Отчество" },
   { id: "phone", title: "Телефон" },
   { id: "email", title: "Email" },
   { id: "address", title: "Адрес" },
+];
+
+const TEXTFIELD_BELPOST_DATA = [
+  ...DEFAULT_TEXTFIELD_DATA,
   { id: "index", title: "Индекс" },
   { id: "comment", title: "Комментарий к заказу" },
 ];
 
+const TEXTFIELD_COURIER_DATA = [
+  ...DEFAULT_TEXTFIELD_DATA,
+  { id: "comment", title: "Комментарий к заказу" },
+];
+
 const TEXTFIELD_EUROPOST_DATA = [
-  { id: "name", title: "Фамилия Имя Отчество" },
-  { id: "phone", title: "Телефон" },
-  { id: "email", title: "Email" },
-  { id: "address", title: "Адрес" },
+  ...DEFAULT_TEXTFIELD_DATA,
+  { id: "address-post", title: "Адрес отделения" },
   { id: "comment", title: "Комментарий к заказу" },
 ];
 
 export const TEXTFIELD_DATA = {
-  courier: TEXTFIELD_EUROPOST_DATA,
+  courier: TEXTFIELD_COURIER_DATA,
   europost: TEXTFIELD_EUROPOST_DATA,
   belpost: TEXTFIELD_BELPOST_DATA,
 }
